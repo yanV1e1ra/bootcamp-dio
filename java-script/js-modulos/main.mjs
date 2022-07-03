eef2144afae19c2078136dd32c3c8a31cdefeecf
Yan Vieira <yan10cvieira@gmail.com>
2022-07-03 14:25:06 -0300

    Atividade sobre módulos

---
+++ java-script/js-modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Yan", 19));
+console.log(mostraCidade("Yan", "Jacareí"));
+console.log(mostraHobby("Yan", "jogar video games"));
